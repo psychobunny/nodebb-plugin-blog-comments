76a2daca5106941941ff2a5e8cf652f6d81fb220
Alex <theunknownartisthour@gmail.com>
2016-03-23 05:02:30 -0700

    Update library.js

--- library.js
+++ library.js
@@ -114,10 +114,10 @@
 			content: content
 		}, function(err, postData) {
 			if(err) {
-				return res.redirect(url + '?error=' + err.message + '#nodebb/comments');
+				return res.redirect(url + '?error=' + err.message + '#nodebb-comments');
 			}
 
-			res.redirect(url + '#nodebb/comments');
+			res.redirect(url + '#nodebb-comments');
 		});
 	};
 
@@ -168,7 +168,7 @@
 						}
 						
 						db.setObjectField('blog-comments', commentID, result.postData.tid);
-						res.redirect((req.header('Referer') || '/') + '#nodebb/comments');
+						res.redirect((req.header('Referer') || '/') + '#nodebb-comments');
 					});
 				} else {
 					res.json({error: "Unable to post topic", result: result});
