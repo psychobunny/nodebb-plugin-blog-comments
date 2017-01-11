485586fd4ac808478e08834e5f5f4677565f34a6
river <revir.qing@gmail.com>
2017-01-11 20:51:08 +0800

    update simple moell.js

--- public/lib/simple-moell.js
+++ public/lib/simple-moell.js
@@ -1,377 +1,77 @@
 (function() {
     "use strict";
 
-    var articlePath = window.location.protocol + '//' + window.location.host + window.location.pathname;
+    function loadScript(url, callback){
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+
+        if (script.readyState && callback){  //IE
+            script.onreadystatechange = function(){
+                if (script.readyState == "loaded" ||
+                        script.readyState == "complete"){
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else if (callback) {  //Others
+            script.onload = function(){
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    function loadCSS(url) {
+        var stylesheet = document.createElement("link");
+        stylesheet.setAttribute("rel", "stylesheet");
+        stylesheet.setAttribute("type", "text/css");
+        stylesheet.setAttribute("href", url);
+        document.getElementsByTagName("head")[0].appendChild(stylesheet);
+    }
+
+    var nbb = {};
+
     var scriptDiv = document.getElementById('nodebb-comments-script');
     var articleEl = document.getElementsByClassName('markdown-body')[0];
 
-    var ourl = scriptDiv.getAttribute('ourl');
-    var ocid = scriptDiv.getAttribute('ocid');
-    var blogger = scriptDiv.getAttribute('blogger');
+    nbb.url = scriptDiv.getAttribute('ourl');
+    nbb.cid = scriptDiv.getAttribute('ocid');
+    nbb.blogger = scriptDiv.getAttribute('blogger');
 
     // get article's title in url.
     var rg = location.pathname.match(/article\/(\d+)/);
-    var articleUTitle = rg && rg.length > 1 ? rg[1]: null;
+    nbb.articleID = rg && rg.length > 1 ? rg[1]: null;
 
-    if (!articleEl || !articleUTitle) {
+    if (!articleEl || !nbb.articleID) {
         console.warn("It's not the article page");
         return;
     }
 
     // get title and content from the article
-    var articleTitle = document.title;
-    var articleContent = articleEl.innerText.split('\n\n').slice(0,2).join('\n\n');
+    nbb.articleTitle = document.title;
+    nbb.articleContent = articleEl.innerText.split('\n\n').slice(0,2).join('\n\n');
 
-    if (!ourl || !ocid || !articleUTitle || !blogger || !articleTitle || !articleContent) {
-        console.warn('information is imcomplete.', ourl, ocid, articleUTitle, blogger, articleTitle, articleContent);
+    if (!nbb.url || !nbb.cid || !nbb.articleID || !nbb.blogger || !nbb.articleContent) {
+        console.warn('[nodebb-plugin-blog-comments2] information is imcomplete.', nbb);
         return;
     } else {
-        console.log('information: ', ourl, ocid, articleUTitle, blogger, articleTitle, articleContent);
+        console.log('[nodebb-plugin-blog-comments2] information: ', nbb);
     }
 
-    var pluginURL = ourl + '/plugins/nodebb-plugin-blog-comments2',
-        savedText, nodebbDiv, contentDiv, commentsDiv, commentsCounter, commentsAuthor, commentsCategory;
+    loadCSS(nbb.url + '/plugins/nodebb-plugin-blog-comments2/css/comments2.css');
 
-    var stylesheet = document.createElement("link");
-    stylesheet.setAttribute("rel", "stylesheet");
-    stylesheet.setAttribute("type", "text/css");
-    stylesheet.setAttribute("href", pluginURL + '/css/comments-pelican.css');
+    // moell's blog don't has jQuery, can't fix youtube embed video right now.
 
-    document.getElementsByTagName("head")[0].appendChild(stylesheet);
     var commentPositionDiv = document.getElementById('nodebb-comments');
     if (!commentPositionDiv) {
         commentPositionDiv = document.createElement('div');
         commentPositionDiv.setAttribute('id', 'nodebb-comments');
         articleEl.parentElement.insertBefore(commentPositionDiv, document.getElementsByClassName('ds-thread')[0]);
     }
-    commentPositionDiv.insertAdjacentHTML('beforebegin', '<div id="nodebb"></div>');
-    nodebbDiv = document.getElementById('nodebb');
 
-    function newXHR() {
-        try {
-            return XHR = new XMLHttpRequest();
-        } catch (e) {
-            try {
-                return XHR = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {
-                return XHR = new ActiveXObject("Msxml2.XMLHTTP");
-            }
-        }
-    }
-
-    var XHR = newXHR(), pagination = 0, modal;
-
-    function authenticate(type) {
-        savedText = contentDiv.value;
-        modal = window.open(ourl + "/" + type + "/#blog/authenticate","_blank","toolbar=no, scrollbars=no, resizable=no, width=600, height=675");
-        var timer = setInterval(function() {
-            if(modal.closed) {
-                clearInterval(timer);
-                pagination = 0;
-                reloadComments();
-            }
-        }, 500);
-    }
-
-    function normalizePost(post) {
-        return post.replace(/href="\/(?=\w)/g, 'href="' + ourl + '/')
-                .replace(/src="\/(?=\w)/g, 'src="' + ourl + '/');
-    }
-
-    XHR.onload = function() {
-        if (XHR.status >= 200 && XHR.status < 400) {
-            var data = JSON.parse(XHR.responseText), html;
-
-            commentsDiv = document.getElementById('nodebb-comments-list');
-            commentsCounter = document.getElementById('nodebb-comments-count');
-            commentsAuthor = document.getElementById('nodebb-comments-author');
-            commentsCategory = document.getElementById('nodebb-comments-category');
-
-            data.relative_path = ourl;
-            data.redirect_url = articlePath;
-            data.article_id = articleUTitle;
-            data.pagination = pagination;
-            data.postCount = parseInt(data.postCount, 10);
-
-            for (var post in data.posts) {
-                if (data.posts.hasOwnProperty(post)) {
-                    data.posts[post].timestamp = timeAgo(parseInt(data.posts[post].timestamp), 10);
-                    if (data.posts[post]['blog-comments:url']) {
-                        delete data.posts[post];
-                    }
-                }
-            }
-
-            if (commentsCounter) {
-                commentsCounter.innerHTML = data.postCount ? (data.postCount - 1) : 0;
-            }
-
-            if (commentsCategory && data.category) {
-                commentsCategory.innerHTML = '<a href="' + ourl + '/category/' + data.category.slug + '">' + data.category.name + '</a>';
-            }
-
-            if (commentsAuthor && data.mainPost) {
-                commentsAuthor.innerHTML = '<span class="nodebb-author"><img src="' + data.mainPost.user.picture + '" /> <a href="' + ourl + '/user/' + data.mainPost.user.userslug + '">' + data.mainPost.user.username + '</a></span>';
-            }
-
-            if (pagination) {
-                html = normalizePost(parse(data, templates.blocks['posts']));
-                commentsDiv.innerHTML = commentsDiv.innerHTML + html;
-            } else {
-                html = parse(data, data.template);
-                nodebbDiv.innerHTML = normalizePost(html);
-            }
-
-            contentDiv = document.getElementById('nodebb-content');
-
-            setTimeout(function() {
-                var lists = nodebbDiv.getElementsByTagName("li");
-                for (var list in lists) {
-                    if (lists.hasOwnProperty(list)) {
-                        lists[list].className = '';
-                    }
-                }
-            }, 100);
-
-            if (savedText) {
-                contentDiv.value = savedText;
-            }
-
-            if (data.tid) {
-                var loadMore = document.getElementById('nodebb-load-more');
-                loadMore.onclick = function() {
-                    pagination++;
-                    reloadComments();
-                }
-                if (data.posts.length) {
-                    loadMore.style.display = 'inline-block';
-                }
-
-                if (pagination * 10 + data.posts.length + 1 >= data.postCount) {
-                    loadMore.style.display = 'none';
-                }
-
-                if (typeof jQuery !== 'undefined' && jQuery() && jQuery().fitVids) {
-                    jQuery(nodebbDiv).fitVids();
-                }
-
-                if (data.user && data.user.uid) {
-                    var error = window.location.href.match(/error=[\S]*/);
-                    if (error) {
-                        if (error[0].indexOf('too-many-posts') !== -1) {
-                            error = 'Please wait before posting so soon.';
-                        } else if (error[0].indexOf('content-too-short') !== -1) {
-                            error = 'Please post a longer reply.';
-                        }
-
-                        document.getElementById('nodebb-error').innerHTML = error;
-                    }
-                } else {
-                    document.getElementById('nodebb-register').onclick = function() {
-                        authenticate('register');
-                    };
-
-                    document.getElementById('nodebb-login').onclick = function() {
-                        authenticate('login');
-                    }
-                }
-            } else {
-                if (data.isAdmin) {
-                    var markdown = articleContent + '\n\n**Click [here]('+articlePath+') to see the full blog post**';
-
-                    document.getElementById('nodebb-content-title').value = articleTitle;
-                    document.getElementById('nodebb-content-markdown').value = markdown;
-                    document.getElementById('nodebb-content-cid').value = ocid;
-                    document.getElementById('nodebb-content-blogger').value = blogger;
-                    //TODO: set tags.
-                    // document.getElementById('nodebb-content-tags').value = JSON.stringify(tags);
-                }
-            }
-        }
-    };
-
-
-
-
-    function reloadComments() {
-        XHR.open('GET', ourl + '/comments/get/' + blogger + '/' + articleUTitle + '/' + pagination, true);
-        XHR.withCredentials = true;
-        XHR.send();
-    }
-
-    reloadComments();
-
-
-    function timeAgo(time){
-        var time_formats = [
-            [60, 'seconds', 1],
-            [120, '1 minute ago'],
-            [3600, 'minutes', 60],
-            [7200, '1 hour ago'],
-            [86400, 'hours', 3600],
-            [172800, 'yesterday'],
-            [604800, 'days', 86400],
-            [1209600, 'last week'],
-            [2419200, 'weeks', 604800],
-            [4838400, 'last month'],
-            [29030400, 'months', 2419200],
-            [58060800, 'last year'],
-            [2903040000, 'years', 29030400]
-        ];
-
-        var seconds = (+new Date() - time) / 1000;
-
-        if (seconds < 10) {
-            return 'just now';
-        }
-
-        var i = 0, format;
-        while (format = time_formats[i++]) {
-            if (seconds < format[0]) {
-                if (!format[2]) {
-                    return format[1];
-                } else {
-                    return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ago';
-                }
-            }
-        }
-        return time;
-    }
-
-    var templates = {blocks: {}};
-    function parse (data, template) {
-        function replace(key, value, template) {
-            var searchRegex = new RegExp('{' + key + '}', 'g');
-            return template.replace(searchRegex, value);
-        }
-
-        function makeRegex(block) {
-            return new RegExp("<!--[\\s]*BEGIN " + block + "[\\s]*-->[\\s\\S]*<!--[\\s]*END " + block + "[\\s]*-->", 'g');
-        }
-
-        function makeConditionalRegex(block) {
-            return new RegExp("<!--[\\s]*IF " + block + "[\\s]*-->([\\s\\S]*?)<!--[\\s]*ENDIF " + block + "[\\s]*-->", 'g');
-        }
-
-        function getBlock(regex, block, template) {
-            data = template.match(regex);
-            if (data == null) return;
-
-            if (block !== undefined) templates.blocks[block] = data[0];
-
-            var begin = new RegExp("(\r\n)*<!-- BEGIN " + block + " -->(\r\n)*", "g"),
-                end = new RegExp("(\r\n)*<!-- END " + block + " -->(\r\n)*", "g"),
-
-            data = data[0]
-                .replace(begin, "")
-                .replace(end, "");
-
-            return data;
-        }
-
-        function setBlock(regex, block, template) {
-            return template.replace(regex, block);
-        }
-
-        var regex, block;
-
-        return (function parse(data, namespace, template, blockInfo) {
-            if (!data || data.length == 0) {
-                template = '';
-            }
-
-            function checkConditional(key, value) {
-                var conditional = makeConditionalRegex(key),
-                    matches = template.match(conditional);
-
-                if (matches !== null) {
-                    for (var i = 0, ii = matches.length; i < ii; i++) {
-                        var conditionalBlock = matches[i].split(/<!-- ELSE -->/);
-
-                        var statement = new RegExp("(<!--[\\s]*IF " + key + "[\\s]*-->)|(<!--[\\s]*ENDIF " + key + "[\\s]*-->)", 'gi');
-
-                        if (conditionalBlock[1]) {
-                            // there is an else statement
-                            if (!value) {
-                                template = template.replace(matches[i], conditionalBlock[1].replace(statement, ''));
-                            } else {
-                                template = template.replace(matches[i], conditionalBlock[0].replace(statement, ''));
-                            }
-                        } else {
-                            // regular if statement
-                            if (!value) {
-                                template = template.replace(matches[i], '');
-                            } else {
-                                template = template.replace(matches[i], matches[i].replace(statement, ''));
-                            }
-                        }
-                    }
-                }
-            }
-
-            for (var d in data) {
-                if (data.hasOwnProperty(d)) {
-                    if (typeof data[d] === 'undefined') {
-                        continue;
-                    } else if (data[d] === null) {
-                        template = replace(namespace + d, '', template);
-                    } else if (data[d].constructor == Array) {
-                        checkConditional(namespace + d + '.length', data[d].length);
-                        checkConditional('!' + namespace + d + '.length', !data[d].length);
-
-                        namespace += d + '.';
-
-                        var regex = makeRegex(d),
-                            block = getBlock(regex, namespace.substring(0, namespace.length - 1), template);
-
-                        if (block == null) {
-                            namespace = namespace.replace(d + '.', '');
-                            continue;
-                        }
-
-                        var numblocks = data[d].length - 1,
-                            i = 0,
-                            result = "";
-
-                        do {
-                            result += parse(data[d][i], namespace, block, {iterator: i, total: numblocks});
-                        } while (i++ < numblocks);
-
-                        namespace = namespace.replace(d + '.', '');
-                        template = setBlock(regex, result, template);
-                    } else if (data[d] instanceof Object) {
-                        template = parse(data[d], d + '.', template);
-                    } else {
-                        var key = namespace + d,
-                            value = typeof data[d] === 'string' ? data[d].replace(/^\s+|\s+$/g, '') : data[d];
-
-                        checkConditional(key, value);
-                        checkConditional('!' + key, !value);
-
-                        if (blockInfo && blockInfo.iterator) {
-                            checkConditional('@first', blockInfo.iterator === 0);
-                            checkConditional('!@first', blockInfo.iterator !== 0);
-                            checkConditional('@last', blockInfo.iterator === blockInfo.total);
-                            checkConditional('!@last', blockInfo.iterator !== blockInfo.total);
-                        }
-
-                        template = replace(key, value, template);
-                    }
-                }
-            }
-
-            if (namespace) {
-                var regex = new RegExp("{" + namespace + "[\\s\\S]*?}", 'g');
-                template = template.replace(regex, '');
-                namespace = '';
-            } else {
-                // clean up all undefined conditionals
-                template = template.replace(/<!-- ELSE -->/gi, 'ENDIF -->')
-                                    .replace(/<!-- IF([^@]*?)ENDIF([^@]*?)-->/gi, '');
-            }
-
-            return template;
-
-        })(data, "", template);
-    }
+    loadScript(nbb.url + '/plugins/nodebb-plugin-blog-comments2/lib/common.js', function () {
+        blogComments2Common(commentPositionDiv, nbb);
+    });
 }());
